6742d41744c7157119059b41537cf9d4dc78f1cd
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-09-06 13:42:07 +0200

    added providers aggregation

--- src/aggregation/runners/provider.runner.ts
+++ src/aggregation/runners/provider.runner.ts
@@ -2,6 +2,8 @@ import { PrismaService } from 'src/db/prisma.service';
 import { PrismaDmobService } from 'src/db/prismaDmob.service';
 import { AggregationRunner } from '../aggregation-runner';
 import { AggregationTable } from '../aggregation-table';
+import { Prisma } from '../../../prisma/generated/client';
+import SortOrder = Prisma.SortOrder;
 
 export class ProviderRunner implements AggregationRunner {
   getName(): string {
@@ -19,7 +21,30 @@ export class ProviderRunner implements AggregationRunner {
     prismaService: PrismaService,
     prismaDmobService: PrismaDmobService,
   ): Promise<void> {
-    // todo: implement
-    console.log('retrieving and storing Providers');
+    const providers = await prismaDmobService.dc_allocation_claim.findMany({
+      select: {
+        providerId: true,
+        clientId: true,
+      },
+      where: {
+        termStart: {
+          gt: 0,
+        },
+        removed: false,
+      },
+      distinct: ['providerId'],
+      orderBy: [{ providerId: SortOrder.asc }, { termStart: SortOrder.asc }],
+    });
+
+    await prismaService.providers.deleteMany({});
+
+    await prismaService.providers.createMany({
+      data: providers.map((dmobProvider) => {
+        return {
+          provider: `f0${dmobProvider.providerId}`,
+          first_client: `f0${dmobProvider.clientId}`,
+        };
+      }),
+    });
   }
 }
