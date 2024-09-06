1ad48f0ae293dce340b9aba983537a1ac8dd9fc2
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-09-06 16:13:52 +0200

    implemented AggregatedClientDealsRunner

--- src/aggregation/runners/aggregated-client-deals.runner.ts
+++ src/aggregation/runners/aggregated-client-deals.runner.ts
@@ -8,8 +8,40 @@ export class AggregatedClientDealsRunner implements AggregationRunner {
     prismaService: PrismaService,
     prismaDmobService: PrismaDmobService,
   ): Promise<void> {
-    // todo: implement
-    console.log('retrieving and storing AggregatedClientDeals');
+    const result = await prismaDmobService.$queryRaw<
+      {
+        client: string;
+        term_start_from: number;
+        term_start_to: number;
+        total_deal_size: number;
+      }[]
+    >`
+        with aggregates as (select 'f0' || "clientId"      as client,
+                                   "termStart" * 30 / 3600 as term_start,
+                                   sum("pieceSize")        as total_deal_size
+                            from dc_allocation_claim
+                            where "termStart" > 0
+                              and removed = false
+                            group by 1, 2)
+        select client                           as "client",
+               term_start * 3600 / 30           as "term_start_from",
+               (term_start + 1) * 3600 / 30 - 1 as "term_start_to",
+               total_deal_size::bigint          as "total_deal_size"
+        from aggregates
+    `;
+
+    await prismaService.aggregated_client_deals.deleteMany({});
+
+    await prismaService.aggregated_client_deals.createMany({
+      data: result.map((dmobResult) => {
+        return {
+          client: dmobResult.client,
+          term_start_from: dmobResult.term_start_from,
+          term_start_to: dmobResult.term_start_to,
+          total_deal_size: dmobResult.total_deal_size,
+        };
+      }),
+    });
   }
 
   getFilledTables(): AggregationTable[] {
@@ -17,7 +49,7 @@ export class AggregatedClientDealsRunner implements AggregationRunner {
   }
 
   getDependingTables(): AggregationTable[] {
-    return [AggregationTable.Providers, AggregationTable.ReplicaDistribution];
+    return [];
   }
 
   getName(): string {
