63da23c8fac76ddf08a22fb6daac988e9050ffe5
neti-mateusz-mazur <mateusz.mazur@neti-soft.com>
2024-09-10 15:38:55 +0200

    jobs sync (#3)

--- src/aggregation/aggregation-tasks.service.ts
+++ src/aggregation/aggregation-tasks.service.ts
@@ -5,15 +5,30 @@ import { AggregationService } from './aggregation.service';
 @Injectable()
 export class AggregationTasksService {
   private readonly logger = new Logger(AggregationTasksService.name);
+  private aggregationJobInProgress = false;
 
   constructor(private readonly aggregationService: AggregationService) {}
 
   @Cron(CronExpression.EVERY_HOUR)
   async runAggregationJob() {
-    this.logger.debug('Starting Aggregations');
+    if (!this.aggregationJobInProgress) {
+      this.aggregationJobInProgress = true;
 
-    await this.aggregationService.runAggregations();
+      try {
+        this.logger.debug('Starting Aggregations');
 
-    this.logger.debug('Finished Aggregations');
+        await this.aggregationService.runAggregations();
+
+        this.logger.debug('Finished Aggregations');
+      } catch (err) {
+        this.logger.error(`Error during Aggregations job: ${err}`);
+      }
+
+      this.aggregationJobInProgress = false;
+    } else {
+      this.logger.debug(
+        'Aggregations job still in progress - skipping next execution',
+      );
+    }
   }
 }
