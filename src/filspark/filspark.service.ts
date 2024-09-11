72125efa70687b720e0f73eb9bdbc517189c66da
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-09-11 14:51:28 +0200

    Move RetrievabilityInfo to dedicated DTO file

--- src/filspark/filspark.service.ts
+++ src/filspark/filspark.service.ts
@@ -3,26 +3,20 @@ import { HttpService } from '@nestjs/axios';
 import { catchError, lastValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
 import { DateTime } from 'luxon';
-
-interface RetrievabilityInfo {
-  miner_id: string;
-  total: string;
-  successful: string;
-  success_rate: number;
-}
+import { RetrievabilityInfoDto } from 'src/types/retrievabilityInfo.dto';
 
 @Injectable()
 export class FilSparkService {
   private readonly logger = new Logger(FilSparkService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  async fetchRetrievability(date: DateTime): Promise<RetrievabilityInfo[]> {
+  async fetchRetrievability(date: DateTime): Promise<RetrievabilityInfoDto[]> {
     const dateParam = date.toFormat('yyyy-MM-dd');
     const endpoint =
       'https://stats.filspark.com/miners/retrieval-success-rate/summary';
     const { data } = await lastValueFrom(
       this.httpService
-        .get<RetrievabilityInfo[]>(endpoint, {
+        .get<RetrievabilityInfoDto[]>(endpoint, {
           params: {
             from: dateParam,
             to: dateParam,
---
+++ src/types/retrievabilityInfo.dto.ts
@@ -0,0 +1,6 @@
+export class RetrievabilityInfoDto {
+  miner_id: string;
+  total: string;
+  successful: string;
+  success_rate: number;
+}
