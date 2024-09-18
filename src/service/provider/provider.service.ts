7becb43d7d1d84c8badbb9ed86bac0d541ccd55a
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-09-18 15:21:07 +0200

    cleanup

--- src/service/provider/provider.service.ts
+++ src/service/provider/provider.service.ts
@@ -14,50 +14,27 @@ export class ProviderService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getProviderClients() {
-    const providerCount = await this.prismaService.$queryRaw<
-      [
-        {
-          count: number;
-        },
-      ]
-    >`select count(distinct provider)::int
-      from client_provider_distribution_weekly`;
-
-    const results = await this.prismaService.$queryRawTyped(
-      getProviderClientsWeekly(),
-    );
-
-    const resultsByWeek = groupBy(results, (p) => p.week);
-
-    const histogramWeekDtos: HistogramWeekDto[] = [];
-    for (const key in resultsByWeek) {
-      const value = resultsByWeek[key];
-      const weekResponses = value.map((r) => {
-        return new HistogramDto(
-          r.value_from_exclusive,
-          r.value_to_inclusive,
-          r.count,
-        );
-      });
-      histogramWeekDtos.push(
-        new HistogramWeekDto(
-          new Date(key),
-          weekResponses,
-          weekResponses.reduce(
-            (partialSum, response) => partialSum + response.count,
-            0,
-          ),
-        ),
-      );
-    }
-
-    return new HistogramWeekResponseDto(
-      providerCount[0].count,
-      histogramWeekDtos,
+    return this.getWeeklyHistogramResult(
+      await this.prismaService.$queryRawTyped(getProviderClientsWeekly()),
     );
   }
 
   async getProviderBiggestClientDistribution() {
+    return this.getWeeklyHistogramResult(
+      await this.prismaService.$queryRawTyped(
+        getProviderBiggestClientDistribution(),
+      ),
+    );
+  }
+
+  private async getWeeklyHistogramResult(
+    results: {
+      value_from_exclusive: number | null;
+      value_to_inclusive: number | null;
+      count: number | null;
+      week: Date;
+    }[],
+  ) {
     const providerCount = await this.prismaService.$queryRaw<
       [
         {
@@ -66,10 +43,6 @@ export class ProviderService {
       ]
     >`select count(distinct provider)::int
       from client_provider_distribution_weekly`;
-
-    const results = await this.prismaService.$queryRawTyped(
-      getProviderBiggestClientDistribution(),
-    );
 
     const resultsByWeek = groupBy(results, (p) => p.week);
 
