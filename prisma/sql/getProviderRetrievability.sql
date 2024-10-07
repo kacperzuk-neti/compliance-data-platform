13fd22d83ca2f3095bead448f31949eb61f70c24
Mateusz Mazur <mateusz.mazur@neti-soft.com>
2024-10-07 14:49:16 +0200

    getProviderRetrievability switched to providers_weekly

--- prisma/sql/getProviderRetrievability.sql
+++ prisma/sql/getProviderRetrievability.sql
@@ -1,8 +1,7 @@
-select
-    100*ceil(success_rate *20)/20 - 5 as "valueFromExclusive",
-    100*ceil(success_rate *20)/20     as "valueToInclusive",
-    count(*)::int                     as "count",
-    date_trunc('week', date)          as "week"
-from provider_retrievability_daily
-group by 1, 2,4
+select 100 * ceil(avg_retrievability_success_rate * 20) / 20 - 5 as "valueFromExclusive",
+       100 * ceil(avg_retrievability_success_rate * 20) / 20     as "valueToInclusive",
+       count(*)::int                                             as "count",
+       week                                                      as "week"
+from providers_weekly
+group by 1, 2, 4
 order by 1;
